9822717906a967ae31e771c8d0185d3b34c46696
peng <peng.nullptr@qq.com>
2025-04-19 10:58:12 +0800

    实现服务端请求处理超时机制

--- geerpc/server.go
+++ geerpc/server.go
@@ -3,6 +3,7 @@ package geerpc
 import (
 	"encoding/json"
 	"errors"
+	"fmt"
 	"gee-rpc/codec"
 	"io"
 	"log"
@@ -108,7 +109,7 @@ func (server *Server) serveCodec(cc codec.Codec) {
 			continue
 		}
 		wg.Add(1)
-		go server.handleRequest(cc, req, sending, wg)
+		go server.handleRequest(cc, req, sending, wg, DefaultOption.HandleTimeout)
 	}
 	wg.Wait()
 	_ = cc.Close()
@@ -163,16 +164,37 @@ func (server *Server) sendResponse(cc codec.Codec, h *codec.Header,
 }
 
 func (server *Server) handleRequest(cc codec.Codec, req *request,
-	sending *sync.Mutex, wg *sync.WaitGroup) {
-	// TODO, should call registered rpc methods to get the right replyv
-	// day 1, just print argv and send a hello message
+	sending *sync.Mutex, wg *sync.WaitGroup, timeout time.Duration) {
+	// 服务端处理报文超时
 	defer wg.Done()
-	err := req.svc.call(req.mType, req.argv, req.replyv)
-	if err != nil {
-		req.h.Error = err.Error()
-		server.sendResponse(cc, req.h, invalidRequest, sending)
+	called := make(chan struct{})
+	sent := make(chan struct{})
+	go func() {
+		// 调用方法超时
+		err := req.svc.call(req.mType, req.argv, req.replyv)
+		called <- struct{}{}
+		if err != nil {
+			req.h.Error = err.Error()
+			server.sendResponse(cc, req.h, invalidRequest, sending)
+		}
+
+		// 发送报文超时
+		server.sendResponse(cc, req.h, req.replyv, sending)
+		sent <- struct{}{}
+	}()
+
+	if timeout == 0 {
+		<-called
+		<-sent
+		return
 	}
-	server.sendResponse(cc, req.h, req.replyv.Interface(), sending)
+	select {
+	case <-time.After(timeout):
+		req.h.Error = fmt.Sprintf("rpc server: request handle timeout: expect within %s", timeout)
+		server.sendResponse(cc, req.h, invalidRequest, sending)
+	case <-called:
+		<-sent
+	}
 }
 
 func (server *Server) Register(rcvr interface{}) error {
