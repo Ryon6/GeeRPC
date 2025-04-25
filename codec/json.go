76694d3124baf745fa704b6cbf9bf8034e3d52a5
peng <peng.nullptr@qq.com>
2025-04-26 05:55:23 +0800

    feat: 新增JSON编解码

---
+++ codec/json.go
@@ -0,0 +1,58 @@
+package codec
+
+import (
+	"bufio"
+	"encoding/json"
+	"io"
+	"log"
+)
+
+type JsonCodec struct {
+	conn io.ReadWriteCloser //  TCP 或者 Unix 建立 socket 时得到的链接实例
+	buf  *bufio.Writer
+	dec  *json.Decoder
+	enc  *json.Encoder
+}
+
+var _ Codec = (*GobCodec)(nil)
+
+func NewJsonCodec(conn io.ReadWriteCloser) Codec {
+	buf := bufio.NewWriter(conn)
+	return &JsonCodec{
+		conn: conn,
+		buf:  buf,
+		dec:  json.NewDecoder(conn),
+		enc:  json.NewEncoder(conn),
+	}
+}
+
+func (c *JsonCodec) ReadHeader(h *Header) error {
+	return c.dec.Decode(h)
+}
+
+func (c *JsonCodec) ReadBody(body interface{}) error {
+	return c.dec.Decode(body)
+}
+
+func (c *JsonCodec) Write(h *Header, body interface{}) (err error) {
+	defer func() {
+		_ = c.buf.Flush()
+		if err != nil {
+			_ = c.Close()
+		}
+	}()
+
+	if err := c.enc.Encode(h); err != nil {
+		log.Println("rpc codec: gob error encoding header:", err)
+		return err
+	}
+	if err := c.enc.Encode(body); err != nil {
+		log.Println("rpc codec: gob error encoding body:", err)
+		return err
+	}
+	return nil
+}
+
+func (c *JsonCodec) Close() error {
+	return c.conn.Close()
+}
